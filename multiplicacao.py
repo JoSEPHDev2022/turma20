aa5bf8364cfc296fcaaf76df2d46dd525403a86c
LuizFdev <jl_ferreira16@hotmail.com>
2022-06-23 11:07:22 -0300

    Create multiplicacao.py
    Adicionando função de multiplicação em outro file

---
+++ multiplicacao.py
@@ -0,0 +1,9 @@
+def multiDoisValores(a,b):
+	return a * b
+
+print("Olá pessoal, vamos multiplicar?")
+
+num1 = int(input('Valor do primeiro número: '))
+num2 = int(input('Valor do segundo número: '))
+
+print(multiDoisValores(num1, num2))
